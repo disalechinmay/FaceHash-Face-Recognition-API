60f61be685c73164d22424b8d3f20e58a9cd39c0
disalechinmay <disalechinmay@gmail.com>
2018-09-14 14:39:10 +0530

    Basic recognition and training done. What about flexibility to the developer?

--- server.py
+++ server.py
@@ -9,13 +9,219 @@ CORS(app)
 def train_POST():
 	import tensorflow as tf
 	tf.keras.backend.clear_session()
+	
+	API_KEY = request.form.get('API_KEY')
+	# Check if API_KEY is correct
+	API_KEY_CORRECT_FLAG = False
+	lines = [line.rstrip('\n') for line in open('API_KEYS')]
+	for line in lines:
+		tempList = line.split()
+		if(tempList[0] == API_KEY):
+			API_KEY_CORRECT_FLAG = True
+
+	if(API_KEY_CORRECT_FLAG == False):
+		return jsonify(
+			responseType = "ERROR",
+			errorDescription = "API_KEY is invalid!"
+		)
+
+
+	incomingUserName = request.form.get('userName')
+
+
+	print("[LOG] -> Started retrieving images...")
 
 	incomingStream = request.form.get('stream')
-	print(incomingStream)
+	#print(incomingStream)
 	splitted = incomingStream.split("[IMAGE_STREAM_DELIMITER]")
-	print(len(splitted))
+	print("[LOG] -> All images received")
+	print("[LOG] -> No of images received : {}".format(len(splitted)))
 
-	return "TRUE"
+	import cv2
+	from imutils import face_utils
+	import numpy as np
+	import argparse
+	import imutils
+	import dlib
+	import math
+	from PIL import Image
+	from subprocess import call
+	import os
+	import array
+	import io
+	import base64 
+	import numpy as np
+	import tensorflow as tf
+	from tensorflow import keras
+	os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+	# Average out all the values:
+
+	# Basically finds distance between 2 points
+	# Arguments:
+	# 	-> tempshape: DLIB's predictor which plots facial landmark points
+	# 	-> point1 & point2: Points between which distance is to be found out
+	def getDistance(tempshape, point1, point2):
+		point1x = tempshape.part(point1).x
+		point1y = tempshape.part(point1).y
+		point2x = tempshape.part(point2).x
+		point2y = tempshape.part(point2).y
+					
+		dx = (point2x - point1x) ** 2
+		dy = (point2y - point1y) ** 2
+		distance = math.sqrt(dx + dy)
+		return distance
+
+	#Importing Haar cascade and DLIB's facial landmarks detector
+	face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+	predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+	targetPoints = []
+
+	# Read map.txt -> Can be tuned!
+	# map.txt holds a collection of points which will be used to recognize a face
+	# map.txt holds a list of pairs between which will define a set of lines to be considered by recognizer
+	lines = [line.rstrip('\n') for line in open('map.txt')]
+	for line in lines:
+		tempList = line.split()
+		targetPoints.append(int(tempList[0]))
+
+	# Holds number of ratios as defined by the map
+	totalTargets = int(len(targetPoints))
+
+	#TOTAL LANDMARKS DETECTED ARE FROM 0-67
+	totals = []
+	counts = []
+	for i in range(0, totalTargets):
+		totals.append(0.0)
+		counts.append(0)
+
+	# Take in base64 string and return PIL image
+	def stringToImage(base64_string):
+		imgdata = base64.b64decode(base64_string)
+		return Image.open(io.BytesIO(imgdata))
+
+	# convert PIL Image to an RGB image( technically a numpy array ) that's compatible with opencv
+	def toRGB(image):
+		return cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+
+
+	def stringToBase64(s):
+		return base64.b64encode(s.encode('utf-8'))
+
+	print("[LOG] -> Started analysing images...")
+	for x in splitted :
+		frame = toRGB(stringToImage(base64.decodestring(stringToBase64(x))))
+
+		#Convert the frame to grayscale
+		grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+		#Activating Haar cascade classifier to detect faces
+		faces = face_cascade.detectMultiScale(grayFrame, scaleFactor = 1.5, minNeighbors = 5)
+
+		for(x, y, w, h) in faces :
+			#Cropping and resizing face area
+			pillowImage = Image.fromarray(frame[y:y+h, x:x+w])
+
+			#Resizing dimensions
+			resizedHeight = 300
+			resizedWidth = 300
+			######
+
+			faceCropped = np.array(pillowImage.resize((resizedHeight, resizedWidth), Image.ANTIALIAS))
+
+			#Initialize dlib's rectangle to start plotting points over shape of the face
+			dlibRect = dlib.rectangle(0, 0, resizedHeight, resizedWidth)
+			shape = predictor(cv2.cvtColor(faceCropped, cv2.COLOR_BGR2GRAY), dlibRect)
+			shapeCopy = shape
+			shape = face_utils.shape_to_np(shape)
+
+			baseLine = getDistance(shapeCopy, 28, 27)
+		
+			for z in range(0, totalTargets):
+				temp = getDistance(shapeCopy, targetPoints[z], 27)
+				currentRatio = float(temp)/float(baseLine)
+
+				prevTotal = totals[z]
+				prevCount = counts[z]
+				totals.remove(totals[z])
+				counts.remove(counts[z])
+				totals.insert(z, prevTotal+currentRatio)
+				counts.insert(z, prevCount+1)
+	print("[LOG] -> All images analyzed.")
+
+	curatedRatios = []
+
+	print("[LOG] -> Calculating average...")
+	if(counts[0] > 50):
+		for x in range(0, totalTargets):
+			averageValue = totals[x]/counts[x]
+			curatedRatios.append(float(averageValue))
+	else:
+		return jsonify(
+				responseType = "FAILURE",
+				failureDescription = "For this to work, at least 50 photos should be sent such that a face will be clearly visible in each one of them."
+			)
+
+	print("[LOG] -> Calculation done.")
+	print(curatedRatios)
+
+	X = []
+	temp = [5.67469951822, 3.01786596509, 3.38644243788, 4.40128707661, 5.6145555955, 6.68856446685, 7.81805994584, 8.67407231537, 1.0, 3.80675806429, 3.97654258777, 3.86155507889, 1.45054649363, 2.74933829225, 2.09696092969, 3.15649919849, 1.45481561561, 5.07613841469, 4.9357296169, 5.17252304232, 6.09019191374, 6.25542463203, 6.29758693006, 5.57055012231, 5.52019500597]
+	X.append(temp)
+	temp = []
+	temp = [7.05974839689, 2.97178072716, 4.64961742495, 5.87977804004, 7.6391815478, 8.55121423004, 10.1963025189, 11.1066475765, 1.0, 4.6929554937, 4.6669834091, 4.62911288534, 2.03025617987, 3.78119038028, 2.98099811072, 4.11835476181, 1.59723153112, 6.73064885914, 6.62688608762, 7.03251560028, 7.96567209384, 7.9931544814, 7.99209303569, 7.27813176265, 7.26395709561]
+	X.append(temp)
+	temp = []
+	temp = [5.94817618968, 2.98047185593, 3.94338878977, 5.10215932554, 6.35623406457, 6.93546084672, 8.35728836014, 8.90828390808, 1.0, 3.98996990559, 4.14730012975, 4.03618344427, 1.68730537362, 3.23670006881, 2.48855973959, 3.76408782268, 1.69563679774, 5.43784904142, 5.29101793332, 5.66541122962, 6.53131476292, 6.56471608808, 6.5759556029, 5.90557981513, 5.86617895934]
+	X.append(curatedRatios)
+	X.append(temp)
+	temp = []
+	temp = [7.6438755024, 2.96538201425, 4.85876054204, 6.37478439871, 7.84219382332, 9.14543948112, 10.7819584314, 11.2441548848, 1.0, 4.78171347933, 4.85244813287, 4.78617682862, 2.04350583494, 3.94124976952, 3.04815828402, 4.53756179591, 1.89986316624, 6.71392975227, 6.55156968539, 7.04501472427, 8.19822194073, 8.25487553558, 8.25869210105, 7.39306423257, 7.35406102551]
+	X.append(temp)
+	temp = []
+	temp = [5.37248726908, 2.95805709001, 3.70943721381, 4.80418983791, 6.09163278029, 6.51618303084, 8.20167385455, 9.44411794871, 1.0, 4.00296728087, 4.20753664349, 4.06320568822, 1.48156586826, 3.00480162014, 2.28002887488, 3.39484461186, 1.24542587064, 5.26059671456, 5.14116816545, 5.506589011, 6.55024284479, 6.70325189921, 6.72756961838, 5.96935584939, 5.91056433697]
+
+	X = np.array(X)
+	y = np.array([[0], [1], [2], [3], [4]])
+
+	model = keras.Sequential()
+	model.add(keras.layers.Dense(50, input_dim=25, activation='softmax'))
+	model.add(keras.layers.Dense(25, activation='softmax'))
+	model.add(keras.layers.Dense(5, activation='softmax'))
+
+	class EarlyStoppingByLossVal(keras.callbacks.Callback):
+	    def __init__(self, monitor='loss', value=0.01, verbose=0):
+	        super(keras.callbacks.Callback, self).__init__()
+	        self.monitor = monitor
+	        self.value = value
+	        self.verbose = verbose
+
+	    def on_epoch_end(self, epoch, logs={}):
+	        current = logs.get(self.monitor)
+	        if current is None:
+	            print("Early stopping requires %s available!" % self.monitor)
+	            exit()
+
+	        if current < self.value:
+	            if self.verbose > 0:
+	                print("Epoch %05d: early stopping THR" % epoch)
+	            self.model.stop_training = True
+	earlyStop = [EarlyStoppingByLossVal()]
+
+	model.compile(loss='sparse_categorical_crossentropy', optimizer='adam')
+
+	model.fit(X, y, batch_size=1, epochs=100000, callbacks=earlyStop)
+
+	os.chdir("/home/chinmay/MASTERS/Flask/HOME:" + API_KEY + "/")
+	model.save(incomingUserName + ".keras")
+	os.chdir("/home/chinmay/MASTERS/Flask/")
+
+
+	return jsonify(
+			responseType = "SUCCESS", 
+			successDescription = "Model trained successfully."
+		)
 
 @app.route('/recognize/', methods=['POST'])
 def recognize_POST():
@@ -188,6 +394,7 @@ def recognize_POST():
 				if(maxid == 2):
 					confidence = kerasOutput[0][2]
 					print("[LOG] -> RETURNING SUCCESS...")
+					print("\nKERAS O/P: {}".format(kerasOutput))
 					return jsonify(
 						responseType = "SUCCESS",
 						successDescription = "Face recognized successfully",
@@ -196,6 +403,7 @@ def recognize_POST():
 					)
 				else:
 					print("[LOG] -> RETURNING FAILURE...")
+					print("\nKERAS O/P: {}".format(kerasOutput))
 					return jsonify(
 						responseType = "FAILURE",
 						failureDescription = "Face not recognized",
